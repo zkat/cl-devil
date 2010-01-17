0f3a58c48c355c4b3f40b46120e127ed694f0c83
Ralith <ralith@gmail.com>
2010-01-17 12:09:32 -0800

    Moved a function of local interest to local scope.

--- utilities.lisp
+++ utilities.lisp
@@ -1,32 +1,31 @@
 (in-package :cl-devil)
 
-(defun w-i-args-helper (args)
-  (when args
-    `((bind-image it)
-      ,(cons
-        (cond ((= 1 (length args)) 'il:load-image)
-              ((= 3 (length args)) 'il:load-l)
-              ((stringp (second args)) 'il:load)
-              (t 'il:load-f))
-        args))))
-
 ;;; XXX I don't like the potential confusion between WITH-BOUND-IMAGE
 ;;; and WITH-IMAGES, but WITH-NEW-IMAGES and WITH-LOADED-IMAGES all
 ;;; give the wrong impression, alas.
 (defmacro with-images ((&rest images) &body body)
   "Generates an IL image for each of IMAGES, binding and loading if a parameter is supplied.  BODY is executed, and the images are freed thereafter."
-  (let ((ids (gensym "IDS"))
-        (count (length images)))
-    `(cffi:with-foreign-object (,ids :uint ,count)
-       (%gen-images ,count ,ids)
-       (unwind-protect
-            (let (,@(loop for x in images
-                       for (var . args) = (if (listp x) x (list x))
-                       for i from 0
-                       collect `(,var (anaphora:aprog1 (cffi:mem-aref ,ids :uint ,i)
-                                        ,@(w-i-args-helper args)))))
-              ,@body)
-         (%delete-images ,count ,ids)))))
+  (flet ((args-helper (args)
+           (when args
+             `((bind-image it)
+               ,(cons
+                 (cond ((= 1 (length args)) 'il:load-image)
+                       ((= 3 (length args)) 'il:load-l)
+                       ((stringp (second args)) 'il:load)
+                       (t 'il:load-f))
+                 args)))))
+   (let ((ids (gensym "IDS"))
+         (count (length images)))
+     `(cffi:with-foreign-object (,ids :uint ,count)
+        (%gen-images ,count ,ids)
+        (unwind-protect
+             (let (,@(loop for x in images
+                        for (var . args) = (if (listp x) x (list x))
+                        for i from 0
+                        collect `(,var (anaphora:aprog1 (cffi:mem-aref ,ids :uint ,i)
+                                         ,@(args-helper args)))))
+               ,@body)
+          (%delete-images ,count ,ids))))))
 
 (defmacro with-init (&body body)
   `(progn (init)
